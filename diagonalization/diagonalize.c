27de1c776ea1df1d98e75a593b0f0e4edab35c03
Eric Rojo <ericrojo156@hotmail.com>
2020-05-30 15:04:07 -0700

    first implementation attempt at Jacobi method for diagonalization. TODO: implement method matmul() for matrix multiplication in diagonalize.c, and write tests for diagonalize()

--- diagonalization/diagonalize.c
+++ diagonalization/diagonalize.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #define M 6
 
-typedef struct svd {
+typedef struct svd_struct {
     int** U;
     int** S;
     int** V;
@@ -27,6 +27,84 @@ void initIdentityMatrix(int** matrix) {
     }
 }
 
+void matmul(int** m1, int** m2, int** dest) {
+    // apply matrix multiplication m1 x m2, and store the resulting matrix in dest
+}
+
+void applyRotations(SVD svd, int i, int j, int theta_l, int theta_r) {
+    int cos_l = cos(theta_l);
+    int sin_l = sin(theta_l);
+    int cos_r = cos(theta_r);
+    int sin_r = sin(theta_r);
+
+    int** _U = (int**)malloc(sizeof(int**) * M);
+    int** _V = (int**)malloc(sizeof(int**) * M);
+    initIdentityMatrix(_U);
+    initIdentityMatrix(_V);
+
+    _U[i][i] = cos_l;
+    _U[j][j] = cos_l;
+    _U[j][i] = sin_l;
+    _U[i][j] = -1 * sin_l;
+
+    _V[i][i] = cos_r;
+    _V[j][j] = cos_r;
+    _V[j][i] = -1 * sin_r;
+    _V[i][j] = cos_r;
+
+    int** _U_t = transpose(_U);
+    int** _V_t = transpose(_V);
+    int** V_t = transpose(svd.V);
+
+
+    int** U = (int**)malloc(sizeof(int**) * M);
+    int** S = (int**)malloc(sizeof(int**) * M);
+    int** S_ = (int**)malloc(sizeof(int**) * M);
+    int** V = (int**)malloc(sizeof(int**) * M);
+    
+    matmul(svd.U, _U_t, U);
+
+    matmul(_U, svd.S, S_);
+    matmul(S_, _V_t, S);
+
+    matmul(_V, V_t, V);
+
+    svd.U = U;
+    svd.S = S;
+    svd.V = V;
+}
+
+void applyJacobiMethod(SVD svd, int i, int j) {
+        int m_ji = svd.S[j][i];
+        int m_ij = svd.S[i][j];
+        int m_ii = svd.S[i][i];
+        int m_jj = svd.S[j][j];
+
+        int X_sum = (m_ji + m_ij)/(m_jj - m_ii);
+        int X_diff = (m_ji - m_ji)/(m_jj + m_ii);
+
+        int theta_sum = arctan(X_sum);
+        int theta_diff = arctan(X_diff);
+        int theta_r = theta_sum + theta_diff;
+        int theta_l = theta_sum - theta_r;
+
+        applyRotations(svd, i, j, theta_l, theta_r);
+}
+
+void sweep(int row, int col, SVD svd) {
+    int** S = svd.S;
+    if (col == M) {
+        return;
+    } else {
+        applyJacobiMethod(svd, row, col);
+        sweep(++row, ++col, svd);
+    }
+}
+
+void startSweeps(SVD svd) {
+    sweep(0, 1, svd);
+}
+
 SVD diagonalize(int** matrix) {
     SVD svd;
     
@@ -34,15 +112,14 @@ SVD diagonalize(int** matrix) {
     int** S = matrix;
     int** V = (int**)malloc(sizeof(int**) * M);
 
-    // stubbed out: initialize U, S, and V to zero matrices
-    // TODO: implement Jacobi method to find values for U, S, and V
     initIdentityMatrix(U);
-
     initIdentityMatrix(V);
 
     svd.U = U;
     svd.S = S;
     svd.V = V;
+
+    startSweeps(svd);
 
     return svd;
 }
