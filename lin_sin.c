5e5908e91840d5de3a9b63dc049b6024a96171dd
seylm <milesransaw@gmail.com>
2020-05-29 18:01:20 -0600

    added linear sine approx and minor fixes to cos approx

--- lin_sin.c
+++ lin_sin.c
@@ -7,7 +7,7 @@
 
 int main(void) {
     // calculate points of theta for linear approximation
-    int points[4] = {-pow(2,N_BITS-1), -pow(2,N_BITS-2), pow(2,N_BITS-2), pow(2,N_BITS-1)-1};
+    int points[3] = {-pow(2,N_BITS-1), 0, pow(2,N_BITS-1)-1};
     
     // calculate slopes of lines
     int slope = 1;
@@ -16,7 +16,7 @@ int main(void) {
     //int slope = (N_BITS_OUT) / (N_BITS_IN); 
     
     // calculate y-intercepts of lines
-    int y_int = pow(2,N_BITS-1);
+    int y_int = pow(2,N_BITS-2);
    
     int theta, result;
     
@@ -32,12 +32,9 @@ int main(void) {
         
         if (theta < points[1]) {
             printf("Theta is less than: %d\n", points[1]);
-            result = -slope * theta - y_int;
+            result = slope * theta + y_int;
         } else if (theta < points[2]) {
             printf("Theta is less than: %d\n", points[2]);
-            result = slope * theta;
-        } else if (theta <= points[3]) {
-            printf("Theta is less than: %d\n", points[3]);
             result = -slope * theta + y_int;
         } else {
             printf("WARNING: theta must be between -pi and pi\n");
