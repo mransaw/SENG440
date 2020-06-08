9eeeec88f3f5885ce4947613c6c70dbc31da5095
seylm <milesransaw@gmail.com>
2020-06-08 17:42:22 -0600

    removed overflow from lin_cos

--- math_utils/lin_cos.c
+++ math_utils/lin_cos.c
@@ -16,7 +16,7 @@ int lin_cos(int theta) {
     int slope = (1<<16);
 
     // magnitude of y-intercepts is 2, scaled
-    int y_int = (2<<30)-1;
+    int y_int = 7FFFFFFF;
     
     if (theta < points[0]) {
         result = -slope * t_theta - y_int;
