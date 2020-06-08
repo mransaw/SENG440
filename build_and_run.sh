994de3286e5696f90dd46f1c2447f11ab835caf3
seylm <milesransaw@gmail.com>
2020-06-08 17:14:57 -0600

    added -lm to build scripts

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,6 +2,6 @@
 rm -rf bin
 mkdir bin
 #gcc math_utils/error_range.c math_utils/arctan.c -o bin/approx_error
-gcc svd_main.c diagonalization/diagonalize.c math_utils/arctan.c math_utils/lin_cos.c math_utils/lin_sin.c -o bin/svd_main -Imath_utils -Idiagonalization
+gcc svd_main.c diagonalization/diagonalize.c math_utils/arctan.c math_utils/lin_cos.c math_utils/lin_sin.c -o bin/svd_main -Imath_utils -Idiagonalization -lm
 
 ./bin/svd_main
