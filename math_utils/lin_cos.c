dfb10307d83e3628ac32d6f2571375088cc7dd9c
seylm <milesransaw@gmail.com>
2020-06-15 14:36:12 -0600

    updated lin_sin/cos

--- math_utils/lin_cos.c
+++ math_utils/lin_cos.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <linear_approx.h>
 
-int lin_sin(int theta) {
+int lin_cos(int theta) {
     int t_theta, result;    
     
     // truncate theta (assumes an input scale factor of (2^31)/pi
