28818db2dd959da5d4ef136b101d75cabaa14f12
seylm <milesransaw@gmail.com>
2020-06-15 14:35:13 -0600

    updated lin_sin/cos

--- math_utils/lin_cos.c
+++ math_utils/lin_cos.c
@@ -3,27 +3,22 @@
 #include <assert.h>
 #include <linear_approx.h>
 
-int lin_cos(int theta) {
+int lin_sin(int theta) {
     int t_theta, result;    
     
     // truncate theta (assumes an input scale factor of (2^31)/pi
     t_theta = (theta>>16);
-
-    // calculate bounds for theta
-    int points[2] = {-(1<<14), (1<<14)};
     
     // magnitude of slopes is 2/pi, scaled
     int slope = (1<<16);
-
-    // magnitude of y-intercepts is 2, scaled
-    int y_int = 0x7FFFFFFF;
     
-    if (theta < points[0]) {
-        result = -slope * t_theta - y_int;
-    } else if (theta < points[1]) {
-        result = slope * t_theta;
+    // magnitude of y-intercepts is 1, scaled
+    int y_int = (1<<30);
+    
+    if (theta < 0) {
+        result = slope * theta + y_int;
     } else {
-        result = -slope * t_theta + y_int;
+        result = -slope * theta + y_int;
     }
     
     // output scale factor is 2^30
