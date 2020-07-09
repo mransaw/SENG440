a4e4493413e6c53485dc856b4e0633d8b15e9535
Eric Rojo <ericrojo156@hotmail.com>
2020-07-09 15:10:41 -0700

    passed arctan tests

--- math_utils/linear_approx.h
+++ math_utils/linear_approx.h
@@ -1,11 +1,8 @@
 #ifndef N_BITS
 #define N_BITS 15
-double arctan_double(double x);
-int decide_method(int X);
+double d_arctan(double x);
+void generate_linear_approximation();
 int arctan(int x);
-int lin_arctan(int x);
-void generate_arctan_table_output();
-int table_interpolation(int X);
 int lin_sin(int theta);
 int lin_cos(int theta);
 #endif
