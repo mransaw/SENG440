f214f830461eb4ae4e19497a441cbfc0efc8c113
Eric Rojo <ericrojo156@hotmail.com>
2020-07-11 18:54:22 -0700

    make intercept more correct

--- math_utils/arctan.c
+++ math_utils/arctan.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <math.h>
 
+double interceptAverageWithinRange(double slope, double lower_x, double upper_x, double step) {
+    double x = lower_x;
+    double accumulate = 0;
+    double n = 1;
+    while (x < upper_x) {
+        double atan_diff = atan(x) - (slope * x);
+        accumulate += atan_diff;
+        x += step;
+        ++n;
+    }
+    return (accumulate / n) * pow(2, SF_ATAN_OUT);
+}
+
 void generate_linear_approximation(double min, double max, double inc) {
     double SCALE_IN = pow(2, SF_ATAN_IN);
     double SCALE_OUT = pow(2, SF_ATAN_OUT);
@@ -12,21 +25,22 @@ void generate_linear_approximation(double min, double max, double inc) {
     double lower_x = min;
     double upper_x = min + inc;
     int i = 0;
+
     while (upper_x <= max) {
         slopes[i] = SLOPE_SF * ((atan(upper_x) - atan(lower_x)) / (upper_x - lower_x));
         double avg_x = (lower_x + upper_x) / 2.0;
         double slope_result = (slopes[i] * (SCALE_IN));
-        double atan_result = atan(avg_x) * SCALE_OUT;
+        double atan_result = atan(avg_x) * SCALE_OUT / M_PI;
         double percent_diff = 100 * fabs(slope_result - atan_result) / atan_result;
         //printf("    slope result = %f, atan result = %f\n, diff = %f, percent diff = %f\n", slope_result, atan_result, slope_result - atan_result, percent_diff);
-        intercepts[i] = atan_result - slope_result;
+        intercepts[i] = interceptAverageWithinRange(slopes[i] / SLOPE_SF, lower_x, upper_x, inc / 5);
         int lower_X = lower_x * SCALE_IN;
         int upper_X = upper_x * SCALE_IN;
         if (lower_x == min) {
-            printf("if (%d <= X && X < %d) { slope *= %d; \n intercept *= %d; }\n", lower_X, upper_X , (int)slopes[i], (int)intercepts[i]);
+            printf("if (%f <= X && X < %f) { \n slope *= %f; \n intercept *= %f; \n}\n", lower_x, upper_x , slopes[i]/SLOPE_SF, intercepts[i]/SCALE_OUT);
         }
         else {
-            printf("else if (%d <= X && X < %d) { slope *= %d; \n intercept *= %d; }\n", lower_X , upper_X , (int)slopes[i], (int)intercepts[i]);
+            printf("else if (%f <= X && X < %f) { \n slope *= %f; \n intercept *= %f; \n}\n", lower_x , upper_x , slopes[i]/SLOPE_SF, intercepts[i]/SCALE_OUT);
         }
         upper_x += inc;
         lower_x += inc;
@@ -37,7 +51,7 @@ void generate_linear_approximation(double min, double max, double inc) {
 void eval_error() {
     double max_error = 0;
     double error = 0;
-    double inc = 0.001;
+    double inc = 0.1;
     for (double x = -1.0; x < 1.0; x += inc) {
         error = (int_arctan(x) - atan(x));
         printf("x = %f, int_arctan(x) = %d, atan(x) = %f, diff error: %f\n", x, int_arctan(x), atan(x), error);
@@ -72,5 +86,21 @@ int int_arctan(int X) {
     double scale_out = pow(2, SF_ATAN_OUT) / M_PI;
     double slope = 1;
     double intercept = 1;
+    if (-16384 <= X && X < -8192) { 
+    slope *= 168689; 
+    intercept *= -2147483648; 
+    }
+    else if (-8192 <= X && X < 0) { 
+    slope *= 243084; 
+    intercept *= -2147483648; 
+    }
+    else if (0 <= X && X < 8192) { 
+    slope *= 243084; 
+    intercept *= -2147483648; 
+    }
+    else if (8192 <= X && X < 16384) { 
+    slope *= 168689; 
+    intercept *= -1884066191; 
+    }
     return (slope * X + intercept);
 }
