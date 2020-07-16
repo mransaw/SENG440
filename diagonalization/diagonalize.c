7ab1fbb542cbbd14edaa4850b652b920017236ca
seylm <milesransaw@gmail.com>
2020-07-16 11:08:35 -0600

    moved functions to diagonalize.h

--- diagonalization/diagonalize.c
+++ diagonalization/diagonalize.c
@@ -187,3 +187,138 @@ void printoutSVD(const SVD svd) {
     printout("V", *svd.V);
     printf("=============================\n");
 }
+
+
+void cordic(int16_t* cos, int16_t* sin, int16_t theta)
+{
+    // initialize sin/cos vector
+    int16_t v[2] = {(1<<SF_ATAN_IN), 0};
+    int16_t angle;
+    
+    for (int j=0; j<ITER; j++) {
+        //int factor;
+        // initialize temporary variables
+        int16_t temp0 = v[0],
+                temp1 = v[1];
+                
+        // perform rotation
+        angle = angles[j];
+        if (theta < 0) {
+            //factor = -(1 >> shiftf);
+            
+            v[0] += (temp1 >> j);
+            v[1] -= (temp0 >> j);
+            theta += angle;
+        } else {
+            //factor = (1 >> shiftf);
+            
+            v[0] -= (temp1 >> j);
+            v[1] += (temp0 >> j);
+            theta -= angle;
+        }
+        
+        //shiftf++;
+        //v[0] = v[0] - factor*v[1];
+        //v[1] = factor*temp + v[1];
+        
+        /*if ((j+1) > ANGLES_LENGTH) {
+            angle = (angle+1) >> 1;
+        } else {
+            angle = angles(j+1);
+        }*/
+    }
+    
+    /*double A = 1;
+    for (int i=0; i<ITER; i++) {
+        A *= sqrt(1 + pow(2,-2*i));
+    }
+    
+    double Kn = 1/A;*/
+    
+    //printf("A: %f, Kn: %f\n", A, Kn);`
+    
+    // apply output factor Kn
+    int temp_cos = v[0] * KN;
+    int temp_sin = v[1] * KN;
+    
+    //printf("input: %d, cos: %d, sin: %d\n", theta, v[0], v[1]); 
+    // remove added SF (from Kn) and return results
+    *cos = (int16_t)((temp_cos + (1<<16))>>17);
+    *sin = (int16_t)((temp_sin + (1<<16))>>17);
+    return;
+}
+
+void dot_productM(int16_t m1[M][M], int16_t m2[M][M], int16_t dest[M][M])
+{
+    int16_t temp[M][M];
+    
+    //print_descaled(m1);
+    //print_descaled(m2);
+    
+    for (int k=0; k<M; k++) {
+        for (int l=0; l<M; l++) {
+            int sum = 0;
+            for (int n=0; n<M; n++) {
+                sum += m1[k][n]*m2[n][l]; // TODO: saturating addition?
+              // printf("1[%d][%d] * 2[%d][%d]\n",k,n,n,l);
+               // printf("sum=%d\n\n, ", sum);
+            }
+            //printf("temp: %d, ",((sum + (SF_ATAN_IN-1)) >> SF_ATAN_IN));
+            temp[k][l] = (int16_t)((sum + (SF_ATAN_IN-1)) >> SF_ATAN_IN);
+        }
+    }        
+    
+    //print_descaled(temp);
+    
+    memcpy(dest, temp, M*M*sizeof(int16_t)); 
+    return;
+}
+
+void transposeM(int16_t source[M][M], int16_t dest[M][M])
+{
+    int16_t result[M][M];
+    
+    for (int k=0; k<M; k++) {
+        for (int l=0; l<M; l++) {
+            result[k][l] = source[l][k];
+        }
+    }
+    memcpy(dest, result, M*M*sizeof(int16_t));
+    return;
+}
+
+void print_matrix2(int16_t matrix[2][2])
+{
+    for (int k=0; k<2; k++) {
+        for (int l=0; l<2; l++) {
+            printf("%d ", matrix[k][l]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return;
+}
+
+void print_matrixM(int16_t matrix[M][M])
+{
+    for (int k=0; k<M; k++) {
+        for (int l=0; l<M; l++) {
+            printf("%d ", matrix[k][l]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return;
+}
+
+void print_descaled(int16_t matrix[M][M])
+{
+    for (int k=0; k<M; k++) {
+        for (int l=0; l<M; l++) {
+            printf("%f ", (double)matrix[k][l]/pow(2,SF_ATAN_IN));
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return;
+}
--- diagonalization/diagonalize.h
+++ diagonalization/diagonalize.h
@@ -1,6 +1,7 @@
 #ifndef M
 #define M 4
 #define NUM_SWEEPS 4
+
 typedef struct svd_struct {
     double*** U;
     double*** S;
@@ -18,3 +19,15 @@ SVD diagonalize(double** matrix);
 void printout(const char * matrixName, double** matrix);
 void printoutSVD(SVD svd);
 #endif
+
+#define ITER 6
+#define KN 79595
+
+extern const int16_t angles[6];
+
+void cordic(int16_t* cos, int16_t* sin, int16_t angle);
+void dot_productM(int16_t m1[M][M], int16_t m2[M][M], int16_t dest[M][M]);
+void transposeM(int16_t source[M][M], int16_t dest[M][M]);
+void print_matrix2(int16_t matrix[2][2]);
+void print_matrixM(int16_t matrix[M][M]);
+void print_descaled(int16_t matrix[M][M]);
