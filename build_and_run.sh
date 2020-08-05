1c435629c67fbfb41ca39f0f90384a9965bffa8b
Eric Rojo <ericrojo156@hotmail.com>
2020-08-05 11:21:55 -0700

    change name of executable

--- build_and_run.sh
+++ build_and_run.sh
@@ -5,8 +5,8 @@ clear
 clear
 
 #gcc int_main.c diagonalization/diagonalize.c math_utils/arctan2.c math_utils/lin_cos.c math_utils/lin_sin.c -o bin/int_main -Imath_utils -Idiagonalization -lm
-gcc int_main.c diagonalization/diagonalize.c math_utils/arctan2.c -o /bin/int_main -Imath_utils -Idiagonalization -lm -ftree-vectorize -mfpu=neon -mcpu=generic-armv7-a -O3
-./bin/int_main
+gcc int_main.c diagonalization/diagonalize.c math_utils/arctan2.c -o bin/testing -Imath_utils -Idiagonalization -lm -ftree-vectorize -mfpu=neon -mcpu=generic-armv7-a -O3
+./bin/testing
 
 #gcc math_utils/error_range.c math_utils/lin_sin.c math_utils/lin_cos.c math_utils/arctan.c -o bin/error_range
 #./bin/error_range
