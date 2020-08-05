7f41be1312107ff4ea9c3a52379ac1384f8cfc33
Eric Rojo <ericrojo156@hotmail.com>
2020-08-05 16:13:28 -0700

    removed conditional branching in arctan2.cordic_arctan() loop, and defined sign() macro

--- math_utils/arctan2.c
+++ math_utils/arctan2.c
@@ -4,36 +4,8 @@
 #include <stdio.h>
 #include <math.h>
 
-#define HALF_UNIT_CIRCLE_DEGREES 180
-#define SCALED_PI 51471 // 3.14 * pow(2, 14)
-#define SCALE_IN (1 << SF_ATAN_IN)
+#define sign(val) (val >= 0) - (val < 0);
 
-/*int int_angles[] = { // radians * pow(2, SF_ATAN_OUT) / pi
-    25736,
-    15193,
-    8027,
-    4075,
-    2045,
-    1024,
-    512,
-    256,
-    128,
-    64,
-    32,
-    16,
-    8,
-    4,
-    2,
-    1
-};*/
-
-int phase_shift(int Y, int X, int z) {
-    int adjusted_z = z;
-    if (X <= 0 && Y <= 0) { // quadrant 3
-        adjusted_z = z + HALF_UNIT_CIRCLE_DEGREES;
-    }
-    return adjusted_z;
-}
 
 int cordic_arctan(int Y, int X) {
     int z = 0;
@@ -47,30 +19,20 @@ int cordic_arctan(int Y, int X) {
     
     int delta_x = 0;
     int delta_y = 0;
-    
+    int signed_angles = 1;
+    int s = 1;
     for (int i = 0; i < ITER; ++i) {
-        if (Y_ >= 0) {
-            delta_x = + (Y_ >> i);
-            delta_y = - (X_ >> i);
-            X_ = X_ + delta_x;
-            Y_ = Y_ + delta_y;
-            z = z + angles[i]; // Q16.15 - Q16.15 --> Q16.15 result
-        } else {
-            delta_x = - (Y_ >> i);
-            delta_y = + (X_ >> i);
-            X_ = X_ + delta_x;
-            Y_ = Y_ + delta_y;
-            z = z - angles[i]; // Q16.15 - Q16.15 --> Q16.15 result
-        }
+        s = sign(Y_);
+        delta_x = s * (Y_ >> i);
+        delta_y = -s * (X_ >> i);
+        X_ = X_ + delta_x;
+        Y_ = Y_ + delta_y;
+        signed_angles = s * (angles[i]);
+        z = z + signed_angles; // Q16.15 - Q16.15 --> Q16.15 result
     }
-    return z;//phase_shift(Y, X, z);
+    return z;
 }
 
 int arctan2(int Y, int X) {
-    int z = 0;
-    /*if (-SCALE_IN < Y && Y < SCALE_IN) {
-        return 0;
-    }*/
-    z = cordic_arctan(Y, X);
-    return z;
+    return cordic_arctan(Y, X);
 }
