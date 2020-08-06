7f2866ea2bc52713f39f7827394f5a01b9a8a0c4
Eric Rojo <ericrojo156@hotmail.com>
2020-08-05 18:22:56 -0700

    use registers and arrange instructions for pipelining

--- math_utils/arctan2.c
+++ math_utils/arctan2.c
@@ -4,22 +4,19 @@
 #include <stdio.h>
 #include <math.h>
 
-#define sign(val) (val >= 0) - (val < 0);
-
 int cordic_arctan(int Y, int X) {
-    int z = 0;
-    int X_ = (X << 14);
+    register int z = 0;
+    register int X_ = (X << 14);
 
-    int Y_ = (Y << 14);
+    register int Y_ = (Y << 14);
     X_ *= sign(X_);
 
     Y_ *= sign(Y_);
-    int delta_x = 0;
+    register int delta_x = 0;
 
-    int s = sign(Y_);
-    int delta_y = 0;
+    register int delta_y = 0;
+    register int s = sign(Y_);
 
-    int signed_angles = 1;
     for (int i = 0; i < ITER; ++i) {
         delta_x = s * (Y_ >> i);
         delta_y = -s * (X_ >> i);
@@ -27,9 +24,7 @@ int cordic_arctan(int Y, int X) {
         X_ = X_ + delta_x;
         Y_ = Y_ + delta_y;
 
-        signed_angles = s * (angles[i]);
-        z = z + signed_angles; // Q16.15 - Q16.15 --> Q16.15 result
-        
+        z = z + (s * (angles[i]));
         s = sign(Y_);
     }
     return z;
