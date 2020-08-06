5dd1f1cae568868f3a43571eeaf9e8d4f139f212
Eric Rojo <ericrojo156@hotmail.com>
2020-08-05 17:26:57 -0700

    rearrange instructions to remove some true dependencies

--- math_utils/arctan2.c
+++ math_utils/arctan2.c
@@ -6,29 +6,31 @@
 
 #define sign(val) (val >= 0) - (val < 0);
 
-
 int cordic_arctan(int Y, int X) {
     int z = 0;
     int X_ = (X << 14);
+
     int Y_ = (Y << 14);
-    
-    if (X_ < 0) {
-        X_ *= -1;
-        Y_ *= -1;
-    }
-    
+    X_ *= sign(X_);
+
+    Y_ *= sign(Y_);
     int delta_x = 0;
+
+    int s = sign(Y_);
     int delta_y = 0;
+
     int signed_angles = 1;
-    int s = 1;
     for (int i = 0; i < ITER; ++i) {
-        s = sign(Y_);
         delta_x = s * (Y_ >> i);
         delta_y = -s * (X_ >> i);
+
         X_ = X_ + delta_x;
         Y_ = Y_ + delta_y;
+
         signed_angles = s * (angles[i]);
         z = z + signed_angles; // Q16.15 - Q16.15 --> Q16.15 result
+        
+        s = sign(Y_);
     }
     return z;
 }
