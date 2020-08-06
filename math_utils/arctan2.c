d147041e7e4e566d17e1365f63781e46a996442f
Eric Rojo <ericrojo156@hotmail.com>
2020-08-06 08:38:41 -0700

    added function calls

--- math_utils/arctan2.c
+++ math_utils/arctan2.c
@@ -51,6 +51,14 @@ void f9() {
 }
 
 int arctan2(int Y, int X) {
+    f1();
+    f2();
+    f3();
+    f4();
+    f5();
+    f6();
+    f7();
+
     register int z = 0;
     register int X_ = (X << 14);
 
